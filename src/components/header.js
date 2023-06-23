2af17b9ea9804e0226bbc7760690ef5edd393385
Jessaroo <j_caetana@yahoo.com>
2023-06-23 15:56:17 -0700

    working on pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,3 @@
-export default function Header(props) {
-  return <h1>Header</h1>;
-}
-
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,31 +1,15 @@
-export default function Projects(props) {
-  return <h1>Projects</h1>;
-}
-
 import { useState, useEffect } from "react";
 
 export default function Projects(props) {
-  // create state to hold projects
   const [projects, setProjects] = useState(null);
-
-  //create function to make api call
   const getProjectsData = async () => {
-    
-		//make api call and get response
     const response = await fetch("./projects.json");
-    
-		// turn response into javascript object
     const data = await response.json();
-    
-		// set the projects state to the data
     setProjects(data);
-
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+  useEffect(() => {getProjectsData()}, []);
 
-  // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
       <div>
