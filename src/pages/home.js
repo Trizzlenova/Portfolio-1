df1fbe60cedf667617a73c40ef1e2c68c0b2d3b2
Jessaroo <j_caetana@yahoo.com>
2023-07-03 21:42:39 -0700

    home page css

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,11 @@ import homeImage from "../images/homePage/homePage.jpeg";
 
 export default function Home(props) {
   return (
-    <div>
-      <h1 className="home-heading">Hello! I am a Full-Stack Software Engineer. Feel free to explore my projects page.</h1>
-      <img src={homeImage} alt="Home" />
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-heading">Hello! I am a Full-Stack Software Engineer. Feel free to explore my projects page.</h1>
+      </div>
+      <img className="home-image" src={homeImage} alt="Home" />
     </div>
   );
 }
